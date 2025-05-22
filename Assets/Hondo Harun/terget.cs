b7e6bb553c61cc61a9fb5a9bf51f90aa89f18b54
Hondo_Harun <harunkun13@gmail.com>
2025-05-22 13:59:25 +0900

    縮小している間スコアが加算される

--- Assets/Hondo Harun/terget.cs
+++ Assets/Hondo Harun/terget.cs
@@ -17,25 +17,28 @@ public class terget : MonoBehaviour
     {
         //下方向に的が移動し続ける
         transform.Translate(Vector3.down * Time.deltaTime);
+        All();
 
     }
     public void All()
     {
-
         //的が時間経過とともに小さくなっていく
         var getsmall = transform.transform.localScale -= Vector3.one * saize * Time.deltaTime;  
         //的の大きさ（X値）がゼロ以下になると・・・・
         if (getsmall.x <= 0) 
         {
+        　　GameManager.Instance.ScorePlus(10);
+            Debug.Log("スコアが上がったよ");
             //的のスケールが0以下になったら消滅する
             Destroy(this.gameObject);
         }
-    }
+        GameManager.Instance.ScorePlus(10);
+        Debug.Log("スコア");
+        }
 
     private void OnBecameInvisible()
     {
         Debug.Log("destroy");
         Destroy(this.gameObject);
-        GameManager.Instance.ScorePlus(10);
     }
 }
