f2392c4d9f5d22cc6f8ee5755c41e5e5f512cf28
rinrin4470 <rinrin4470@gmail.com>
2025-05-22 11:44:40 +0900

    二回目

--- Assets/RintaroSato/play.cs
+++ Assets/RintaroSato/play.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class play : MonoBehaviour
 {
+    //カーソルに使用するテクスチャ
+    [SerializeField]
+    private Texture2D cursor;
     // Start is called before the first frame update
     void Start()
     {
-        
+        //カーソルを自前のカーソルに変更
+        Cursor.SetCursor(cursor, new Vector2(cursor.width / 2, cursor.height / 2), CursorMode.ForceSoftware);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //マウスの左クリックで撃つ
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shot();
+        }
+    }
+    //敵を撃つ
+    void Shot()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if(Physics.Raycast(ray,out hit, 100f, LayerMask.GetMask("Enemy")))
+        {
+            Destroy(hit.collider.gameObject);
+        }
     }
 }
