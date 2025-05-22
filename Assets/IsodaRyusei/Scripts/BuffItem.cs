91bd853fa1f7cbdc41a566d74e5918df0d8dc5d8
isodaryusei <isoda.tatsunari@gmail.com>
2025-05-22 17:10:19 +0900

    [fix]スクリプトを修正

--- Assets/IsodaRyusei/Scripts/BuffItem.cs
+++ Assets/IsodaRyusei/Scripts/BuffItem.cs
@@ -1,11 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class BuffItem : MonoBehaviour
 {
     [SerializeField,Header("スコアの加算値")]
     private int BonusScore = 0;
+    [SerializeField,Header("バフの継続時間")]
+    private float BuffTime;
+    public GameObject Shooter;
 
     // Start is called before the first frame update
     void Start()
@@ -15,6 +19,11 @@ public class BuffItem : MonoBehaviour
 
     // Update is called once per frame
     void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
     {
         
     }
