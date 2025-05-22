809fe08053d714a7b82efd76c675a1b89f81e481
isodaryusei <isoda.tatsunari@gmail.com>
2025-05-22 16:38:47 +0900

    [add]InGameUIのPrehubを追加

---
+++ Assets/IsodaRyusei/Scripts/BuffItem.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuffItem : MonoBehaviour
+{
+    [SerializeField,Header("スコアの加算値")]
+    private int BonusScore = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
