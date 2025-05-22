6fa81fb2735d5963726f50a1039e40dfa610c601
mayarei2662 <mayarei2662@gmail.com>
2025-05-22 15:04:49 +0900

    ランキング画面作成

---
+++ Assets/reiyama/Retry_Button.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class Retry_Button : MonoBehaviour
+{
+
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void OnClick()
+    {
+        Debug.Log("Retry");
+    }
+}
