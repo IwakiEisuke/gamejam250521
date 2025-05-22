8d1c8c306a1a9207e039189a8faf34942e802837
Hondo_Harun <harunkun13@gmail.com>
2025-05-22 14:53:01 +0900

    テスト用の的を作成

--- Assets/Hondo Harun/terget.cs
+++ Assets/Hondo Harun/terget.cs
@@ -7,6 +7,7 @@ using UnityEngine;
 public class terget : MonoBehaviour
 {
     [SerializeField] float saize = 0;
+    [SerializeField] int pulspoint = 0;
     void Start()
     {
 
@@ -23,20 +24,20 @@ public class terget : MonoBehaviour
     }
     public void All()
     {
-        //的が時間経過とともに小さくなっていく
+        //的が小さくなっていく
         var getsmall = transform.transform.localScale -= Vector3.one * saize * Time.deltaTime;  
         //的の大きさ（X値）がゼロ以下になると・・・・
         if (getsmall.x <= 0) 
         {
             //的が消滅するとスコアが加算される
-        　　GameManager.Instance.ScorePlus(10);
+        　　GameManager.Instance.ScorePlus(pulspoint);
             Debug.Log("スコアが上がったよ");
             //的のスケールが0以下になったら消滅する
             Destroy(this.gameObject);
         }
         //的が縮小している間スコアがかさんされる
         GameManager.Instance.ScorePlus(10);
-        Debug.Log("スコア");
+        Debug.Log("スコアが" + pulspoint + "上がった");
         }
 
     private void OnBecameInvisible()
