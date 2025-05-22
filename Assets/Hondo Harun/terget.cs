b4e627113bcaa3bdeccb7eb2256880d4dcebf858
Hondo_Harun <harunkun13@gmail.com>
2025-05-22 12:48:44 +0900

    的の縮小行動とスケール値が0以下になると消滅を追加

--- Assets/Hondo Harun/terget.cs
+++ Assets/Hondo Harun/terget.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.InteropServices;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class terget : MonoBehaviour
 {
     //[SerializeField] string Production = ""; 
     //生成するオブジェクト変数
+    string _terget;
     string _terget_move;
 
     void Start()
@@ -16,7 +18,17 @@ public class terget : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {        transform.Translate(Vector3.down * Time.deltaTime);
+    {
+        //下方向に的が移動し続ける
+        transform.Translate(Vector3.down * Time.deltaTime);
+        //的が時間経過とともに小さくなっていく
+        var getsmall = transform.transform.localScale -= Vector3.one * Time.deltaTime;
+        //的の大きさ（X値）がゼロ以下になると・・・・
+        if (getsmall.x <= 0 );
+        {
+            //的のスケールが0以下になったら消滅する
+            Destroy(this.gameObject);
+        }
 
     }
 }
