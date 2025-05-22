237a40d6fa5e7d6e4499584da933a030010eab12
isodaryusei <isoda.tatsunari@gmail.com>
2025-05-22 11:47:00 +0900

    [add]ScoreManagerスクリプトを追加

---
+++ Assets/IsodaRyusei/Scripts/ScoreManager.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour
+{
+    public int Score = 0;
+    public Text ScoreText;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        UpdateScoreText();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void UpdateScoreText()
+    {
+        ScoreText.text = "Score" + Score.ToString();
+    }
+
+    public void AddScore(int amount)
+    {
+        Score += amount;
+        UpdateScoreText();
+    }
+
+}
