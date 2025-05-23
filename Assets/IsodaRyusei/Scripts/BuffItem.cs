594b90cdd8f689318ecb08b5fb0858c52d2d5fb1
isodaryusei <isoda.tatsunari@gmail.com>
2025-05-23 14:20:48 +0900

    更新

--- Assets/IsodaRyusei/Scripts/BuffItem.cs
+++ Assets/IsodaRyusei/Scripts/BuffItem.cs
@@ -6,6 +6,22 @@ public class BuffItem : MonoBehaviour, IPointerClickHandler
     private float _effectTime;
     [SerializeField, Header("Œø‰Ê”{—¦")]
     private int _effect;
+    [SerializeField]
+    private float _spawnTime = 3;
+
+    private float _timer;
+
+    private void Start()
+    {
+        _timer = 0;
+    }
+
+    private void Update()
+    {
+        _timer += Time.deltaTime;
+        if (_timer > _spawnTime) 
+            Destroy(gameObject);
+    }
 
     public void OnPointerClick(PointerEventData eventData)
     {
