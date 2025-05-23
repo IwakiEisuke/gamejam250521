6218b7c7683f4ef38c3cfaefa6ab6d9cbc2f6f48
isodaryusei <isoda.tatsunari@gmail.com>
2025-05-23 12:10:37 +0900

    更新

--- Assets/IsodaRyusei/Scripts/BuffItem.cs
+++ Assets/IsodaRyusei/Scripts/BuffItem.cs
@@ -3,36 +3,27 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.SocialPlatforms.Impl;
 
 public class BuffItem : MonoBehaviour
 {
-    [SerializeField,Header("スコアの加算値")]
+    [SerializeField, Header("スコアの加算値")]
     private int BonusScore = 0;
     [SerializeField,Header("バフの継続時間")]
     private float BuffTime;
-    public GameObject Shooter;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     private void OnCollisionEnter(Collision collision)
     {
         Bullet bullet = collision.gameObject.GetComponent<Bullet>();
-
-        if (bullet != null )
+        if (bullet != null)
         {
-            GameManager scoreManager = FindObjectOfType<GameManager>();
+            GameManager gm = FindObjectOfType<GameManager>();
+            if (gm != null)
+            {
+                
+            }
         }
-
+        
         Destroy(collision.gameObject);
         Destroy(gameObject);
     }
